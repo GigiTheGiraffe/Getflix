f4fb216a24ff9902ef922f84388b3ff2470ea7b4
Martin Loic <loic.martin2000@gmail.com>
2024-07-30 15:50:46 +0200

    connection

--- getGenre.php
+++ getGenre.php
@@ -1,5 +1,6 @@
 <?php
 require_once('vendor/autoload.php');
+include 'loadEnv.php';
 
 $client = new \GuzzleHttp\Client();
 
@@ -9,5 +10,32 @@ $response = $client->request('GET', 'https://api.themoviedb.org/3/genre/movie/li
     'accept' => 'application/json',
   ],
 ]);
-
-echo $response->getBody();
+$contents = $response->getBody()->getContents();
+//print_r(json_decode($contents));
+$array = (array) $contents[0];
+print_r($array);
+/*
+//echo $response;
+//print_r($contents);
+echo '<br>';
+$array = (array) $contents;
+print_r($response);
+echo '<br>';
+*/
+try {
+// Ouverture connexion
+$conn = new PDO('mysql:host=' . $_ENV['DB_SERVERNAME'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+// Mettre le mode erreur
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Utiliser la co
+$stmt = $conn->prepare('SELECT * FROM genres');
+$stmt->execute();
+$genres = $stmt->fetchAll(PDO::FETCH_ASSOC);
+print_r($genres);
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+  exit;
+}
+// et maintenant, fermez-la !
+$conn = null;
+?>
