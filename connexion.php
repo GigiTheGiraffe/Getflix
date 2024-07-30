9da5c36964548aa5f7bc6d9d81c8d0b0e17ae790
Martin Gausseran <martin.gausseran@gmail.com>
2024-07-30 15:53:36 +0200

    Récupération des fichiers fronts (Movies+Fiche)

---
+++ connexion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<title>Se connecter à Flouflix</title>
+<meta name="description" content="Créez votre compte et accèdez dès aujourd'hui à notre catalogue de films. Déjà membre ? Connectez-vous ici.">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+<h1>Créez votre compte</h1>
+<form>
+<label for="username">Nom de compte</label>
+<input id="username"name="username">
+<label for="password">Mot de passe</label>
+<input id="password" name="password">
+</form>
+</body>
+</html>
