b91e955f51a80dd61a7945d81548715a162fb41e
Martin Loic <loic.martin2000@gmail.com>
2024-08-01 16:19:48 +0200

    fichier célia working

---
+++ famous.php
@@ -0,0 +1,43 @@
+<?php
+require_once('vendor/autoload.php');
+include 'load_env.php';
+loadEnv(__DIR__ . '/.env');
+
+$client = new \GuzzleHttp\Client();
+
+
+$response = $client->request('GET', 'https://api.themoviedb.org/3/trending/movie/day?language=en-US', [
+  'headers' => [
+    'Authorization' => 'Bearer ' . $_ENV['MOVIEDB_TOKEN'],
+    'accept' => 'application/json',
+  ],
+]);
+
+
+$data = json_decode($response->getBody()->getContents(), true);
+
+// Fonction pour mapper les IDs de genres aux noms des genres
+function getGenreName($genreId) {
+  $genres = [
+    28 => 'Action',
+    12 => 'Adventure',
+    16 => 'Animation',
+    35 => 'Comedy',
+    80 => 'Crime',
+    99 => 'Documentary',
+    18 => 'Drama',
+    10751 => 'Family',
+    14 => 'Fantasy',
+    36 => 'History',
+    27 => 'Horror',
+    10402 => 'Music',
+    9648 => 'Mystery',
+    10749 => 'Romance',
+    878 => 'Science Fiction',
+    10770 => 'TV Movie',
+    53 => 'Thriller',
+    10752 => 'War',
+    37 => 'Western'
+  ];
+  return isset($genres[$genreId]) ? $genres[$genreId] : 'Unknown';
+}
