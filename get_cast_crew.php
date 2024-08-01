b0358cadada74ba80fd5b82124a46542c19c6fe1
Martin Loic <loic.martin2000@gmail.com>
2024-08-01 11:52:24 +0200

    get trailer fonctionne, debut de get crew and cast

--- get_cast_crew.php
+++ get_cast_crew.php
@@ -4,14 +4,6 @@ require_once('vendor/autoload.php');
 include 'load_env.php';
 loadEnv(__DIR__ . '/.env');
 
-$client = new \GuzzleHttp\Client();
-
-$response = $client->request('GET', 'https://api.themoviedb.org/3/movie/157336/credits?language=en-US', [
-  'headers' => [
-    'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDg1ZjQ2NWJlYjYzZTQ5ZTkzNWVlNTVhYzQyNDEwZiIsIm5iZiI6MTcyMjMzNTg5OC41Nzg2NDEsInN1YiI6IjY2YTg4ZmUwYWM0MDMyODNhNGYyYTY2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.545yIdGFN9y0ZE8cLVy-1l2g6ZGyacqC_5vsJa8Dh4c',
-    'accept' => 'application/json',
-  ],
-]);
 function getData(int $movieId)
 {
 
