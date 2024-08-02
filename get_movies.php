cf84a10d9ec3bc6bc9821f015f4b6855eba5a7c9
Martin Loic <loic.martin2000@gmail.com>
2024-08-02 14:42:02 +0200

    mise de get movies en commentaire et ajout de page.php et stylesmovies.css pour les lier a la db

--- get_movies.php
+++ get_movies.php
@@ -4,7 +4,7 @@ include 'import_page.php';
 include 'insert_db.php';
 
 // Boucle pour obtenir la réponse JSON de 50 pages, soit 1000 film. Arrete à la page 70
-for ($i = 0; $i < 51; $i++) {
+/*for ($i = 0; $i < 51; $i++) {
 $responses = importPages($i);
 insertIntoDb($responses);
-}
+}*/
