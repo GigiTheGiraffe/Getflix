af7be4effcc5a4749ee13742bbbf3a6e5eb4cd46
Martin Loic <loic.martin2000@gmail.com>
2024-08-02 10:54:05 +0200

    db produite

--- get_movies.php
+++ get_movies.php
@@ -3,8 +3,8 @@ include_once 'load_env.php';
 include 'import_page.php';
 include 'insert_db.php';
 
-// Boucle pour obtenir la réponse JSON de 50 pages, soit 1000 film.
-for ($i = 1; $i < 51; $i++) {
+// Boucle pour obtenir la réponse JSON de 50 pages, soit 1000 film. Arrete à la page 70
+for ($i = 0; $i < 51; $i++) {
 $responses = importPages($i);
 insertIntoDb($responses);
 }
